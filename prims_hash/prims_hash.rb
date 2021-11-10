1f25897b18e97cad010882d100125ad239f91860
SergeyElo <kostroma_sv@mail.ru>
2021-11-10 12:13:23 +0300

    add prim_hash01_edition

--- prims_hash/prims_hash.rb
+++ prims_hash/prims_hash.rb
@@ -6,6 +6,7 @@ DIRS = Hash[*%w{
         /media/Lightning/G/TV\ Storage      TV
     }]
 puts DIRS
+puts DIRS.keys
 
 puts "------- prim 2 ---------" # заполнение и вывод ключей
 h = Hash.new("Go Fish")
@@ -18,10 +19,13 @@ h1 = { "a" => 1, "c" => 2 }
 h2 = { 7 => 35, "c" => 2, "a" => 1 }
 h3 = { "a" => 1, "c" => 2, 7 => 35 }
 h4 = { "a" => 1, "d" => 2, "f" => 35 }
-puts "Hash h1: #{h1}"
-puts "Hash h2: #{h2}"
-puts "Hash h3: #{h3}"
-puts "Hash h4: #{h4}"
+print "Hash h1: #{h1} keys:#{h1.keys}\n"
+print "Hash h2: #{h2} keys:#{h2.keys}\n"
+print "Hash h3: #{h3} keys:#{h3.keys}\n"
+print "Hash h4: #{h4} keys:#{h4.keys}\n"
 puts "h1 == h2 :#{h1 == h2}"
 puts "h2 == h3 :#{h2 == h3}"
 puts "h3 == h4 :#{h3 == h4}"
+
+h1_new = h1.invert
+print "Hash h1_new - invert h1 : #{h1_new} keys:#{h1_new.keys}\n"
