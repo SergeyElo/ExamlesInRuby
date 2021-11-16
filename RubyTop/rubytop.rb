b09fcefc06c6971e99bd4d86d2fc6e3ec00288f4
SergeyElo <kostroma_sv@mail.ru>
2021-11-16 22:43:03 +0300

    add in OOP && RubyTop

--- RubyTop/rubytop.rb
+++ RubyTop/rubytop.rb
@@ -6,3 +6,13 @@ puts m.select { |x| x > 0 }.inject { |s, x| s + x }
 s = "abcdefgh 0123456789"
 p s.codepoints              # p - вместо puts "#{}"
 
+# таблица символов ASCII
+chars = (32..127).map do |ord|
+  k = case ord
+      when 32  then "␠"
+      when 127 then "␡"
+      else ord.chr
+      end
+  "#{ord.to_s.ljust(3)}- #{k}"
+end
+chars.each_slice(chars.size/6).to_a.transpose.each{|s| puts s.join("  ")}
