1b8a9e400149bab8ecf81980bedc74a6e6d7940a
SergeyElo <kostroma_sv@mail.ru>
2021-11-18 12:29:45 +0300

    add reverse numbers

---
+++ RubyTop/reverse_numbers.rb
@@ -0,0 +1,14 @@
+ary = (1..9).to_a
+ary.shuffle! while ary == ary.sort
+score = 0
+until ary == ary.sort
+  print "#{ary.inspect} -- How many digits to reverse? "
+  num = gets.to_i  # should validate input
+  if num == 0
+    exit(0)
+  end
+  ary[0, num] = ary[0, num].reverse
+  score += 1
+end
+p ary
+puts "Your score: #{score}"
