9702d3b854f6ae33da1613cbc8160a57acc32512
SergeyElo <kostroma_sv@mail.ru>
2021-11-19 15:42:19 +0300

    comment auto

--- Zadachi/palindrom.rb
+++ Zadachi/palindrom.rb
@@ -1,5 +1,14 @@
 s = "АРОЗАУПАЛАНАЛАПУАЗОРА"
 
+# old - from another language
+i = 0
+f = true
+while (i <= (s.length - 1) % 2) && f
+  f = s[i] == s[s.length - 1 - i]
+  i += 1
+end
+p f
+
 sr = s.reverse
 p sr
 p s == sr
