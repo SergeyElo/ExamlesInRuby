0fd20e1150a0df1236f3b470d2a26d13f480ca6c
SergeyElo <kostroma_sv@mail.ru>
2021-11-22 15:24:28 +0300

    Edit Folder scan

--- scan/scan.rb
+++ scan/scan.rb
@@ -1,4 +1,9 @@
-s = "Hello! My age 35. sun 234 ddffr 98 fgfgf 4 fhfhfh 5f6 f77hgg0."
-puts "Only numbers: #{s.scan (/[0-9]/)}"
-puts "Char not numbers: #{s.scan (/[^0-9]/)}"
+s = "Hello! My age 35. My phone: 8-910-900-90-90."
+puts "Only numbers: #{s.scan(/[0-9]/)}"
+puts "Char not numbers: #{s.scan(/[^0-9]/)}"
+puts "Only numbers 2: #{s.scan(/\d/)}"
+puts "Char not numbers 2: #{s.scan(/\D/)}"
+puts "Only letters: #{s.scan(/[A-Za-z]/)}"
 
+w = "12 567 7 889,90,8 94 432"
+puts "Numbers: #{w.scan(/[0-9]+ /)}"
