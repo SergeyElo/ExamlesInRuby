b27647cbd6bc3dea03aece73a985c216f30ba51c
SergeyElo <kostroma_sv@mail.ru>
2021-11-23 23:15:15 +0300

    edit in OOP

--- OOP/Classes/prim_class_nasledovanie/p2.rb
+++ OOP/Classes/prim_class_nasledovanie/p2.rb
@@ -1,5 +1,8 @@
 class Animal
   attr_accessor :name, :age, :color
+  def say
+    "rrrrrrrrrrrrrrrrrrrr!"
+  end
 end
 
 class Dog < Animal
@@ -10,6 +13,9 @@ end
 
 class Snake < Animal
   attr_accessor :length
+  def say
+    "s-s-s-s-s..."
+  end
 end
 
 a = Dog.new
@@ -18,7 +24,7 @@ a.age = 3
 a.color = "white"
 
 b = Cat.new
-b.name = "Kim"
+b.name = "Kisa"
 b.age = 1
 b.color = "black"
 
@@ -28,6 +34,8 @@ c.age = 2
 c.color = "gray"
 c.length = 55
 
-puts "#{a.name} #{a.age} #{a.color}"
-puts "#{b.name} #{b.age} #{b.color}"
-puts "#{c.name} #{c.age} #{c.color} #{c.length}"
+puts "#{a.name} #{a.age} #{a.color} #{a.say}"
+puts "#{b.name} #{b.age} #{b.color} #{b.say}"
+puts "#{c.name} #{c.age} #{c.color} #{c.length} #{c.say}"
+
+
